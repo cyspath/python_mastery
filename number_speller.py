bb11d0d66a1a8a4ab4072d14efd4997b4ed24302
Mike <lvlichaelly@gmail.com>
2015-10-14 10:06:37 -0700

    functional number converter

--- number_speller.py
+++ number_speller.py
@@ -14,7 +14,7 @@ def to_a(num):
 def read_three(a):
     set_x = ['','one','two','three','four','five', 'six','seven','eight','nine']
     set_1x = ['','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','ninteen']
-    set_x0 = ['', 1,'twenty','thirty','fourty','fifty','sixty', 'seventy','seven','eight','nine']
+    set_x0 = ['', 1,'twenty','thirty','fourty','fifty','sixty', 'seventy','eighty','ninety']
     result = []
 
     if a[0]!= 0:
@@ -34,26 +34,37 @@ def read_three(a):
 
 # converts a number to english representation
 def spell(num):
-    a = to_a(num)
+    prefix = []
+    if num < 0:
+        prefix = ['negative']
+
+    a = to_a(abs(num))
 
     if len(a)%3 != 0:
         n = 3-(len(a)%3)
         i = 0
-        print(n)
         while i < n:
             a.insert(0, 0)
             i += 1
-    print(a)
 
     result = []
 
-    if len(a) <= 3:
-        return read_three(a)
-    elif len(a) <= 6:
-        return
+    if len(a) == 3:
+        list_with_name = prefix + read_three(a)
+    elif len(a) == 6:
+        list_with_name = prefix + read_three(a[0:3]) + ['thousand'] + read_three(a[3:len(a)])
+    elif len(a) == 9:
+        thousand = []
+        for el in a[3:6]:
+            if el != 0:
+                thousand = ['thousand']
+        list_with_name = prefix + read_three(a[0:3]) + ['million'] + read_three(a[3:6]) + thousand + read_three(a[6:len(a)])
 
+    return " ".join(list_with_name)
 
-print(spell(5))
+print(spell(211000000))
+print(spell(-999999999))
+
 
 def move(n, a, c, b):
     if n > 0:
